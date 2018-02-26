895a7d143dc58c731e7a770dfaaf0ea17b11c474
The Ambassador of Funk <acobster@gmail.com>
2018-02-26 09:11:05 -0800

    more meta-updates to the update script

--- scripts/update.bash
+++ scripts/update.bash
@@ -2,6 +2,10 @@
 
 
 main() {
+  # First make sure we're on master and starting at the latest version we have
+  reset_cwd
+
+  # Establish the Source of Truth
   LATEST_WORDPRESS_CHECKSUM_URL='https://wordpress.org/wordpress-latest.tar.gz.sha1'
   LATEST_WORDPRESS_DOWNLOAD_URL='https://wordpress.org/wordpress-latest.tar.gz'
 
@@ -19,6 +23,15 @@ main() {
     latest_version=$(get_latest_version)
     update $latest_version $latest_checksum
   fi
+}
+
+
+reset_cwd() {
+  git reset --hard
+  git clean -fd
+  git fetch --tags
+  git checkout master
+  git pull origin master
 }
 
 
@@ -54,21 +67,27 @@ update() {
   sed -i '' "s/^ENV WORDPRESS_SHA1 .*/ENV WORDPRESS_SHA1 $latest_checksum/" Dockerfile
   sed -i '' "s/^Current version: .*/Current version: `$latest_version`/" README.md
 
+  # build and tag Docker image
   DOCKER_IMAGE=${DOCKER_IMAGE:-'sitecrafting/lando-wp-theme'}
   docker build -t "$DOCKER_IMAGE:$latest_version" .
   docker tag "$DOCKER_IMAGE:$latest_version" "$DOCKER_IMAGE:latest"
 
+  # push docker image if we can
   if [[ -f .env ]] ; then
     source .env
+
+    # attempt login
     docker login --username $DOCKER_USERNAME --password $DOCKER_PASSWORD
-    docker push "$DOCKER_IMAGE:$latest_version"
-    docker push "$DOCKER_IMAGE:latest"
+    if [[ "$?" ]] ; then
+      docker push "$DOCKER_IMAGE:$latest_version"
+      docker push "$DOCKER_IMAGE:latest"
+    fi
   fi
 
   git checkout -b "v$latest_version" master
   git commit --all -m "bump version to $latest_version"
-  git tag -am "tag $latest_version" "$latest_version"
-  # TODO git push
+  git tag -am "tag for wordpress release $latest_version" "$latest_version"
+  git push origin "v$latest_version"
 }
 
 
